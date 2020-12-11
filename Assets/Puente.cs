e24bf8b441a0d38441087e0cc0a493c19a17dcf6
spergio8 <spergio8@gmail.com>
2020-12-11 20:29:02 -0300

    you was sleeping and i was working

---
+++ Assets/Puente.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puente : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<EdgeCollider2D>().enabled = true;
+                                     
+        }
+
+    }
+}
